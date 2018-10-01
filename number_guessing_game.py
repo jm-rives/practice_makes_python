7dec446105de1d157c1165a381145f2922943536
Joanna M. Rives <joanna.rives@gmail.com>
2018-10-01 12:25:58 -0700

    ADD working function

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,10 +1,24 @@
 import random
 
 def guess_the_number():
-
+    game = True
+    # Choose a random integer between 0 and 99 inclusive
     correct_guess = random.randint(0, 100)
+    guess = input("Enter your guess (whole numbers only) 🐢 ->")
+    int_guess = int(guess)
 
-    pass
+    while game == True:
 
+        if int_guess < correct_guess:
+            print("Your guess was too LOW!")
+            guess = input("Enter your NEW guess (whole numbers only) 🐢 ->")
+            int_guess = int(guess)
+        elif int_guess > correct_guess:
+            print("Your guess was too HIGH!")
+            guess = input("Enter your NEW guess (whole numbers only) 🐢 ->")
+            int_guess = int(guess)
+        else:
+            print("Congratulations! You guessed correctly! 🎉🎉🎉")
+            game = False
 
 guess_the_number()
